7e2587bf7e9cb5771de1781e060478ac63a6145c
Huan Nguyen <huannv@solazu.com>
2024-05-28 15:18:18 +0700

    update todos controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,14 +1,14 @@
 class API::V1::TodosController < API::V1::APIController
   # GET /api/v1/todos
   def index
-		todos = Todo.filter(filter_params)
-    render json: { todos: todos }
+    todos = Todo.filter(filter_params)
+    render json: { todos: }
   end
 
   # GET /api/v1/todos/:id
   def show
     todo = Todo.find(params[:id])
-    render json: { todo: todo }
+    render json: { todo: }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Todo not found' }, status: :not_found
   end
@@ -17,7 +17,7 @@ class API::V1::TodosController < API::V1::APIController
   def create
     todo = Todo.new(todo_params)
     if todo.save
-      render json: { todo: todo }, status: :created
+      render json: { todo: }, status: :created
     else
       render json: { error: todo.errors.full_messages }, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class API::V1::TodosController < API::V1::APIController
   def update
     todo = Todo.find(params[:id])
     if todo.update(todo_params)
-      render json: { todo: todo }
+      render json: { todo: }
     else
       render json: { error: todo.errors.full_messages }, status: :unprocessable_entity
     end
@@ -46,9 +46,9 @@ class API::V1::TodosController < API::V1::APIController
 
   private
 
-	def filter_params
-		params.slice(:status)
-	end
+  def filter_params
+    params.slice(:status)
+  end
 
   def todo_params
     params.require(:todo).permit(:name, :status)
